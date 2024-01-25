b061d634b0122e8b44045b96cbd6a3b832eca6f3
Stephen <stevobialor@gmail.com~>
2024-01-25 04:54:39 +0100

    include additional checks

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ export default function processList(list) {
     if (!Array.isArray(list)) throw new Error('List not an array');
     list.map((el) => {
         if (!(typeof el === 'number')) throw new Error('List must be made up of numbers');
-        if (el % 10 !== 0) throw new Error("integer not a multiple of 10");
+        if (el % 10 !== 0) throw new Error("number not a multiple of 10");
     });
 
     return list.filter((el, index) => !((index % 2 === 0 || index % 3 === 0) && index !== 0));
