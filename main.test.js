ef1f00e7bb52826e82b6666e16dd330ae424a481
Stephen <stevobialor@gmail.com~>
2024-01-26 10:39:59 +0100

    add: test case for non-integer

--- main.test.js
+++ main.test.js
@@ -4,12 +4,15 @@ import processList from './main';
 
 describe('The processList function', () => {
     it('returns the right result when list length is a multiple of 10', () => {
-        const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-        expect(processList(array)).toEqual([1, 2, 6, 8]);
+        const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        expect(processList(array)).toEqual([0, 1, 5, 7]);
     });
 
     it('throws error when list length is not zero and not a multiple of 10', () => {
-        expect(() => processList([1])).toThrowError(new Error('length of list must be a multiple of 10'));
+        const array1 = [0,1,2,3,4,5,6,7,8,9,10,11];
+        const array2 = [0,1];
+        expect(() => processList(array1)).toThrowError(new Error('length of list must be a multiple of 10'));
+        expect(() => processList(array2)).toThrowError(new Error('length of list must be a multiple of 10'));
     });
 
     it('throws an error if no content in list', () => {
@@ -27,8 +30,12 @@ describe('The processList function', () => {
         expect(() => processList()).toThrowError(new Error('You must provide an argument'));
     });
 
-    it('throws an error when argument is a truthy', () => {
-        const array = [10, 20, 30, 40, 50, true, 70, 80, 90, 100];
+    it('throws an error when argument is not an integer', () => {
         expect(() => processList(true)).toThrowError(new Error('argument must be an array'));
+    });
+
+    it('throws an error when argument contains non-integer', () => {
+        const array = [10, 20, 30, 40, 50, true, 70, 80, 90, 100];
+        expect(() => processList(array)).toThrowError(new Error('list must be made up of only integers'));
     });
 });
