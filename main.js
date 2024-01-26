6f084856334d9e33db7a71ee1510f62a2acf31fd
Stephen <stevobialor@gmail.com~>
2024-01-26 11:01:04 +0100

    Fix: optimise code by iterating only once through list

--- main.js
+++ main.js
@@ -9,11 +9,12 @@ export default function processList(list) {
     if (!list) throw new Error('You must provide an argument');
     if (!Array.isArray(list)) throw new Error('argument must be an array');
     if (list.length === 0 || list.length % 10 !== 0) throw new Error('length of list must be a multiple of 10');
-    if (list.some(el => !Number.isInteger(el))) throw new Error('list must be made up of only integers');
 
     let newList = list.slice();
 
-    for (let index = (newList.length - 1); index > 0; index--) {
+    let index;
+    for (index = (newList.length - 1); index > 0; index--) {
+        if (!Number.isInteger(newList[index])) throw new Error('list must be made up of only integers');
         if (index % 2 === 0 || index % 3 === 0) newList.splice(index, 1);
     }
 
