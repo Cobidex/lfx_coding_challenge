f528a0a1dfed77f367f29f847682a6843a907c89
Stephen <stevobialor@gmail.com~>
2024-01-26 10:39:38 +0100

    Add: check for non-integer list element

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /*
   processList - processes a list
 
-  @list: array
+  @list: array of integers
 
   Returns: mutated array or throws an error
 */
@@ -10,5 +10,13 @@ export default function processList(list) {
     if (!Array.isArray(list)) throw new Error('argument must be an array');
     if (list.length === 0 || list.length % 10 !== 0) throw new Error('length of list must be a multiple of 10');
 
-    return list.filter((el, index) => !((index % 2 === 0 || index % 3 === 0) && index !== 0));
+    let newList = list.slice();
+
+    let index;
+    for (index = (newList.length - 1); index > 0; index--) {
+        if (!Number.isInteger(newList[index])) throw new Error('list must be made up of only integers');
+        if (index % 2 === 0 || index % 3 === 0) newList.splice(index, 1);
+    }
+
+    return newList;
 }
